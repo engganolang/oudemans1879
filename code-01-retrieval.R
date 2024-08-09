01a16f096eafb0aede2760fea0112ba18b28080a
Enggano <engganolang@gmail.com>
2024-08-09 22:28:32 +0800

    Update LICENSE and internal code.

--- code-01-retrieval.R
+++ code-01-retrieval.R
@@ -4,8 +4,8 @@ library(reactable)
 
 oudemans1889 <- read_xlsx("../enolex-2023-05-23/wordlist/Oudemans_1889_(Francis_wordlist).xlsx")
 oudemans1889 |> 
-  rename(Francis1870 = Enggano_Francis_1870,
-         vdStraaten_Severijn1855 = vdStraaten_Severijn_1855,
-         vonRosenberg1855 = vonRosenberg_1855,
-         Boewang1854 = Boewang_1854) |> 
+  rename(Francis__1870 = Enggano_Francis_1870,
+         vdStraaten_Severijn__1855 = vdStraaten_Severijn_1855,
+         vonRosenberg__1855 = vonRosenberg_1855,
+         Boewang__1854 = Boewang_1854) |> 
   write_tsv("data/oudemans1889.tsv")
