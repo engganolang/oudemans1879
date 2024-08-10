066d6794597850fe376dc9f46a5590816fd273f2
Gede Rajeg <primahadiwijaya@gmail.com>
2024-08-10 22:36:24 +0800

    Add pre-processing code.

---
+++ code-02-preprocessing.R
@@ -0,0 +1,16 @@
+library(tidyverse)
+
+oudemans1889 <- read_tsv("data/oudemans1889.tsv")
+oudemans1889
+
+oudlong <- oudemans1889 |> 
+  pivot_longer(cols = -c(English, Dutch, Page), names_to = "Source", values_to = "Forms") |> 
+  separate_wider_delim(cols = Source, delim = "__", names = c("Source", "Year")) |> 
+  mutate(Source = factor(Source, 
+                         levels = c("Francis", "vdStraaten_Severijn", 
+                                    "vonRosenberg", "Boewang")),
+         Year = factor(Year,
+                       levels = c("1854", "1855", "1870"))) |> 
+  select(Page, Dutch, English, Enggano = Forms, Year, Source)
+oudlong |> 
+  write_tsv("data/oudemans1889-long.tsv")
