e79680a3780c0d3b28476daae4c9e261b8cff40f
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-01-29 15:02:32 +0800

    Add the .csv for the long format.

--- code-01-retrieval.R
+++ code-01-retrieval.R
@@ -3,10 +3,13 @@ library(tidyverse)
 library(reactable)
 
 oudemans1889 <- read_xlsx("../enolex-2023-05-23/wordlist/Oudemans_1889_(Francis_wordlist).xlsx")
-oudemans1889 |> 
+oudemans1889_1 <- oudemans1889 |> 
   rename(Francis__1870 = Enggano_Francis_1870,
          vdStraaten_Severijn__1855 = vdStraaten_Severijn_1855,
          vonRosenberg__1855 = vonRosenberg_1855,
          Boewang__1854 = Boewang_1854) |> 
-  mutate(across(where(is.character), ~replace_na(., ""))) |> 
+  mutate(across(where(is.character), ~replace_na(., ""))) 
+oudemans1889_1 |> 
   write_tsv("data/oudemans1889.tsv")
+oudemans1889_1 |> 
+  write_csv("data/oudemans1889.csv")
